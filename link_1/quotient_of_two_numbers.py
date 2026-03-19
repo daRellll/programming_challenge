c15b3d09c14d8445c5c36ec28e9f544814f485c7
daRellll <darylljohngsabanal@gmail.com>
2026-03-19 12:40:37 +0000

    fixed a typo in quotient program

--- link_1/quotient_of_two_numbers.py
+++ link_1/quotient_of_two_numbers.py
@@ -1,4 +1,4 @@
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
 
-print("The product of the two numbers are: ", first_number / second_number)
+print("The quotient of the two numbers are: ", first_number / second_number)

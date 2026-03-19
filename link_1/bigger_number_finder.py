9b188f364954d4a4e98704c6fafde011182b4062
daRellll <darylljohngsabanal@gmail.com>
2026-03-19 12:15:05 +0000

    added bigger numer finder

---
+++ link_1/bigger_number_finder.py
@@ -0,0 +1,7 @@
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+if first_number > second_number:
+    print(first_number, "is bigger")
+else:
+    print(second_number, "is bigger")

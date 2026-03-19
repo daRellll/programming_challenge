a9137a0f2c400bc126b96f45314f95db52a8426e
daRellll <darylljohngsabanal@gmail.com>
2026-03-19 12:41:59 +0000

    added a programm for first number raised to the second number

---
+++ link_1/first_number_raised_to_the_second_number.py
@@ -0,0 +1,4 @@
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+
+print("The first number raised by the second number is: ", first_number ** second_number)

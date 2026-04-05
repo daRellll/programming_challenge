9b6ad4ae994539de15a3ca42b538b267aea87591
daRellll <darylljohngsabanal@gmail.com>
2026-04-05 19:40:40 +0800

    added a program that prints the numbers in between the two input numbers

---
+++ link_2/numers_in_between_two_input_numbers_printer.py
@@ -0,0 +1,7 @@
+starting_number = float(input("enter a number to start with: "))
+
+ending_number = float(input("enter a number to end with: "))
+
+while starting_number < ending_number:
+ print(starting_number + 1)
+ starting_number += 1

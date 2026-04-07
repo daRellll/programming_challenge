d1e048bdf8c9a0b02128e86cd9ec4fa4d6036e5b
daRellll <darylljohngsabanal@gmail.com>
2026-04-07 11:49:06 +0800

    optimized the code for identifying the lowest number inputted

--- link_3/lowest_number_identifier_from_inputs.py
+++ link_3/lowest_number_identifier_from_inputs.py
@@ -1,17 +1,12 @@
-lowest_number = None
+numbers = []
 
 while True:
     try:
         user_input = float(input("Enter a number: "))
         
-        if lowest_number is None:
-            lowest_number = user_input
-        elif lowest_number < user_input:
-            pass
-        else:
-            lowest_number = user_input
+        numbers.append(user_input)
             
     except ValueError: 
              break 
 
-print(lowest_number)
+print(min(numbers))

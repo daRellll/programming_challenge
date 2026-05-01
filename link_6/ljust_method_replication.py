9a57199c7440e4016727abc58c8fdabf067da88d
daRellll <darylljohngsabanal@gmail.com>
2026-05-01 17:26:35 +0800

    fixed the ljust replication program to more accurately replicate how ljsut works

--- link_6/ljust_method_replication.py
+++ link_6/ljust_method_replication.py
@@ -1,4 +1,5 @@
 word = input("Enter a word: ")
 length_of_padding = int(input("Enter the amount of padding you want to add: "))
+left_padding = max(0, length_of_padding - len(word))
 
-print(word + " " * length_of_padding)
+print(word + " " * left_padding)

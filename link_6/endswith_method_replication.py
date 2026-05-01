c68e2975b0c8652b5d7106d2c6d29e886d8c72b0
daRellll <darylljohngsabanal@gmail.com>
2026-05-01 17:51:17 +0800

    finished endswith method replication

--- link_6/endswith_method_replication.py
+++ link_6/endswith_method_replication.py
@@ -1,5 +1,17 @@
-user_input = input("Type something: ")
-endswith_parameter = input("Enter what you want to check: ")
+string = input("Enter a string: ")
+suffix = input("Enter the suffix to check: ")
 
-for char in endswith_parameter:
-    
+if len(suffix) == 0:
+    print(True)
+elif len(suffix) > len(string):
+    print(False)
+else:
+    match = True
+    string_index = len(string) - len(suffix)
+
+    for i in range(len(suffix)):
+        if string[string_index + i] != suffix[i]:
+            match = False
+            break
+
+    print(match)

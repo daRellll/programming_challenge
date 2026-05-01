21bcd9521352778c331df712b3e7868c3a33f4ef
daRellll <darylljohngsabanal@gmail.com>
2026-05-01 17:20:30 +0800

    added a program that replicates the ljust() method manually

---
+++ link_6/endswith_method_replication.py
@@ -0,0 +1,5 @@
+user_input = input("Type something: ")
+endswith_parameter = input("Enter what you want to check: ")
+
+for char in endswith_parameter:
+    
---
+++ link_6/ljust_method_replication.py
@@ -0,0 +1,4 @@
+word = input("Enter a word: ")
+length_of_padding = int(input("Enter the amount of padding you want to add: "))
+
+print(word + " " * length_of_padding)

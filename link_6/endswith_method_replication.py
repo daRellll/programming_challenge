6063fe2fd5f54fd7793b9103e6c341dc5f528c7e
daRellll <darylljohngsabanal@gmail.com>
2026-06-06 06:35:56 +0800

    shortened code

--- link_6/endswith_method_replication.py
+++ link_6/endswith_method_replication.py
@@ -3,15 +3,6 @@ suffix = input("Enter the suffix to check: ")
 
 if len(suffix) == 0:
     print(True)
-elif len(suffix) > len(string):
-    print(False)
 else:
-    match = True
-    string_index = len(string) - len(suffix)
-
-    for i in range(len(suffix)):
-        if string[string_index + i] != suffix[i]:
-            match = False
-            break
-
+    match = len(suffix) <= len(string) and string[-len(suffix):] == suffix
     print(match)

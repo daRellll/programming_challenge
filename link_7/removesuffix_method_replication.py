c9ee94d2bdbcafa827e685ed6c465f8ae510a4ae
daRellll <darylljohngsabanal@gmail.com>
2026-06-06 06:15:41 +0800

    shortened code

--- link_7/removesuffix_method_replication.py
+++ link_7/removesuffix_method_replication.py
@@ -1,18 +1,7 @@
 string = input("Enter a string: ")
 suffix = input("Enter suffix to remove: ")
 
-if len(suffix) > len(string):
-    print(string)
-else:
-    start = len(string) - len(suffix)
-    match = True
+start = len(string) - len(suffix)
+match = start >= 0 and all(string[start + i] == suffix[i] for i in range(len(suffix)))
 
-    for i in range(len(suffix)):
-        if string[start + i] != suffix[i]:
-            match = False
-            break
-
-    if match:
-        print(string[:start])
-    else:
-        print(string)
+print(string[:start] if match else string)

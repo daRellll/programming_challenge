3aacec74eaa79f367b674e7c16e3751cec9ea23e
daRellll <darylljohngsabanal@gmail.com>
2026-06-06 06:17:26 +0800

    shortened code

--- link_7/count_method_replication.py
+++ link_7/count_method_replication.py
@@ -2,16 +2,8 @@ string = input("Enter a string: ")
 sub = input("Enter substring to count: ")
 
 count = 0
-
-for start in range(len(string) - len(sub) + 1):
-    match = True
-
-    for i in range(len(sub)):
-        if string[start + i] != sub[i]:
-            match = False
-            break
-
-    if match:
+for i in range(len(string) - len(sub) + 1):
+    if string[i:i+len(sub)] == sub:
         count += 1
 
 print(count)

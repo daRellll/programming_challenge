741467ab379783a911f2c2b1ec53a7e944b9508f
daRellll <darylljohngsabanal@gmail.com>
2026-06-06 05:20:30 +0800

    shortened the code

--- link_7/startswith_method_replication.py
+++ link_7/startswith_method_replication.py
@@ -1,14 +1,4 @@
 string = input("Enter a string: ")
 prefix = input("Enter prefix to check: ")
 
-if len(prefix) > len(string):
-    print(False)
-else:
-    match = True
-
-    for i in range(len(prefix)):
-        if string[i] != prefix[i]:
-            match = False
-            break
-
-    print(match)
+print(len(prefix) <= len(string) and all(string[i] == prefix[i] for i in range(len(prefix))))

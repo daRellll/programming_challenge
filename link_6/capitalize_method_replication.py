cf2953d7676b307723a540113eaa62108c1a3808
daRellll <darylljohngsabanal@gmail.com>
2026-06-06 06:37:45 +0800

    shortened code

--- link_6/capitalize_method_replication.py
+++ link_6/capitalize_method_replication.py
@@ -3,21 +3,5 @@ word = input("Enter a word: ")
 if len(word) == 0:
     print("")
 else:
-    result = ""
-
-    first = word[0]
-    first_ascii = ord(first)
-    if 97 <= first_ascii <= 122:
-        result += chr(first_ascii - 32)
-    else:
-        result += first
-
-    for i in range(1, len(word)):
-        char = word[i]
-        ascii_val = ord(char)
-        if 65 <= ascii_val <= 90:
-            result += chr(ascii_val + 32)
-        else:
-            result += char
-
+    result = word[0].upper() + word[1:].lower()
     print(result)

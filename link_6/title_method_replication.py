c6968708e05a0dd9f276fca266e26d5d53aa39f7
daRellll <darylljohngsabanal@gmail.com>
2026-06-06 06:34:31 +0800

    shortened code

--- link_6/title_method_replication.py
+++ link_6/title_method_replication.py
@@ -3,19 +3,13 @@ result = ""
 new_word = True
 
 for char in word:
-    ascii_val = ord(char)
-    is_upper = 65 <= ascii_val <= 90
-    is_lower = 97 <= ascii_val <= 122
-    is_letter = is_upper or is_lower
-
-    if new_word and is_letter:
-        result += chr(ascii_val - 32) if is_lower else char
-        new_word = False
-    elif is_letter:
-        result += chr(ascii_val + 32) if is_upper else char
-    else:
+    if char == " ":
         result += char
-        if char == " ":
-            new_word = True
+        new_word = True
+    elif new_word:
+        result += char.upper()
+        new_word = False
+    else:
+        result += char.lower()
 
 print(result)

c30c43d38d620560c3900d4eaa3a94b0942ce4a7
daRellll <darylljohngsabanal@gmail.com>
2026-06-06 05:23:53 +0800

    shortened the code

--- link_7/rindex_method_replication.py
+++ link_7/rindex_method_replication.py
@@ -2,16 +2,8 @@ string = input("Enter a string: ")
 sub = input("Enter substring to find: ")
 
 found = -1
-
-for start in range(len(string) - len(sub) + 1):
-    match = True
-
-    for i in range(len(sub)):
-        if string[start + i] != sub[i]:
-            match = False
-            break
-
-    if match:
-        found = start
+for i in range(len(string) - len(sub) + 1):
+    if string[i:i+len(sub)] == sub:
+        found = i
 
 print(found)

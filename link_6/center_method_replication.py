fe69c75ba93c1fd95cf7ec44ecd6c0b640710398
daRellll <darylljohngsabanal@gmail.com>
2026-06-06 06:36:53 +0800

    shortened code

--- link_6/center_method_replication.py
+++ link_6/center_method_replication.py
@@ -2,8 +2,5 @@ word = input("Enter a word: ")
 char_fill = input("Enter a char: ")
 total_width = int(input("Enter how much to add: "))
 
-padding_needed = max(0, total_width - len(word))
-left_padding = padding_needed // 2
-right_padding = padding_needed - left_padding
-
-print(char_fill * left_padding + word + char_fill * right_padding)
+padding = max(0, total_width - len(word))
+print(char_fill * (padding // 2) + word + char_fill * (padding - padding // 2))
